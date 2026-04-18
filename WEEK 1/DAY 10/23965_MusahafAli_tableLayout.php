eb8d446e59224c5d751202953d2fa55c55148c6b
Musahaf Ali <musahafali43@gmail.com>
2026-04-18 07:47:29 +0500

    added 3rd assignment code

---
+++ WEEK 1/DAY 10/23965_MusahafAli_tableLayout.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Table Structure Layout</title>
+</head>
+<body>
+
+<table border="1" width="450" height="310" cellspacing="0" cellpadding="3">
+
+    <tr>
+        <td colspan="5" align="center"><b>Header</b></td>
+    </tr>
+
+    <tr>
+        <td rowspan="4" align="center">Side 1</td>
+        <td colspan="3" align="center">Content 1</td>
+        <td rowspan="4" align="center">Side 2</td>
+    </tr>
+
+    <tr>
+        <td align="center">Img</td>
+        <td align="center">Img</td>
+        <td align="center">Img</td>
+    </tr>
+
+    <tr>
+        <td align="center">Content 2</td>
+        <td align="center" colspan="2" >Content 3</td>
+    </tr>
+
+    <tr>
+        <td colspan="3" align="center">Content 4</td>
+    </tr>
+
+    <tr>
+        <td colspan="5" align="center"><b>Footer</b></td>
+    </tr>
+
+</table>
+
+</body>
+</html>
