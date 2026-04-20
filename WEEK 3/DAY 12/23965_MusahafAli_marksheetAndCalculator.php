37a071725464fe1152507c23a422d949e8a688d4
Musahaf Ali <musahafali43@gmail.com>
2026-04-21 04:38:38 +0500

    I have completed assignment no 1 marksheet and calculator on single page handling

--- WEEK 3/DAY 12/23965_MusahafAli_marksheetAndCalculator.php
+++ WEEK 3/DAY 12/23965_MusahafAli_marksheetAndCalculator.php
@@ -33,12 +33,38 @@
                 <th>Programming:</th>
                 <td><input type="text" name="programming"></td>
             </tr>
-            <tr align=center>
-                
-                <td colspan=2><input type="submit" name="marksheet" value="marksheet"></td>
+            <tr align=center>                
+                <td colspan=2><input type="submit" name="marksheet"></td>
             </tr>
         </table>
     </form>
     </center>
+    <br><br><br>
+    <center>
+    <form action="process.php" method="POST">
+        
+        <table cellspacing=2 cellpadding=10 border=2>
+            <tr>
+                <th colspan=2>CALCULATOR</th>
+            </tr>
+            <tr>
+                <th>Value1:</th>
+                <td><input type="text" name="value1"></td>
+            </tr>
+             <tr>
+                <th>Value2:</th>
+                <td><input type="text" name="value2"></td>
+            </tr>
+             <tr>
+                <th>Operator:</th>
+                <td><input type="text" name="op"></td>
+            </tr>
+            <tr align=center>                
+                <td colspan=2><input type="submit" name="calculator"></td>
+            </tr>
+        </table>
+    </form>
+    </center>
+
 </body>
 </html>
--- WEEK 3/DAY 12/process.php
+++ WEEK 3/DAY 12/process.php
@@ -1,7 +1,106 @@
 <?php
-echo $_POST['submit'];
-if($_POST["submit"] == "marksheet"){
-    echo "Hao baba hao";
+if(isset($_POST["marksheet"])){
+    
+echo "<h1>MARKSHEET ASSIGNMENT </h1>";
+
+$computer = $_POST["computer"] ;
+$economics = $_POST["economics"] ;
+$finance = $_POST["finance"];
+$managment = $_POST["managment"] ;
+$programming = $_POST["programming"];
+$total_marks = 500 ; // considering 100 marks per subject 
+$obtained_marks ;
+$percentage ;
+$grade ;
+
+$obtained_marks = $computer + $finance + $economics + $managment + $programming;
+echo "Obtained Marks = $obtained_marks from $total_marks " ;
+echo "<br />";
+echo "<br />";
+
+$percentage = $obtained_marks / $total_marks * 100 ;
+echo "Percentage of obtained marks = ".$percentage;
+echo "<br />";
+echo "<br />";
+
+// checking if pass or fail according to subjects ...
+$status = ($computer >= 40 && $finance >= 40 && $economics >= 40 && $managment>= 40 && $programming >= 40);
+if($status){
+    echo "You are PASS";
+}else {
+    echo "You are FAIL";
+}
+
+echo "<br />";
+echo "<br />";
+
+// using if else condition to check grade
+// This is first way to do this right......
+if($percentage >= 90 AND $percentage < 100){
+    $grade = "A+";
+} else if($percentage >= 80 AND $percentage < 90){
+    $grade = "A";
+}else if($percentage >= 70 AND $percentage < 80){
+    $grade = "B+";
+}else if($percentage >= 60 AND $percentage < 70){
+    $grade = "B";
+}else if($percentage >= 50 AND $percentage < 60){
+    $grade = "C";
+} else {
+    $grade = "Fail";
+}
+
+echo "Your obtained Grade is : $grade ";
+echo "<br />";
+echo "<br />";
+
+
+// This is second thing we can do......
+if($status){
+    if($percentage >= 90 AND $percentage < 100){
+        $grade = "A+";
+    } 
+    else if($percentage >= 80 AND $percentage < 90){
+        $grade = "A";
+    }else if($percentage >= 70 AND $percentage < 80){
+        $grade = "B+";
+    }else if($percentage >= 60 AND $percentage < 70){
+        $grade = "B";
+    }else if($percentage >= 50 AND $percentage < 60){
+        $grade = "C";
+    } else {
+        $grade = "Fail";
+    }
+    echo "Your obtained Grade is : $grade ";
+    echo "<br />";
+    echo "<br />";
+}else{
+    echo "You not elegible for Grades!!!!!!";
+}
+
 };
 
+if(isset($_POST["calculator"])){
+    //*********** CALCULATOR ****************
+
+$number1 = $_POST['value1'];
+$number2 = $_POST['value2'];
+$operator = $_POST['op'];
+$result = null;
+
+
+if($operator === '/'){
+    $result = "Division of  $number1 / $number2 = ".($number1/$number2);
+}else if($operator === '*'){
+    $result = "Multiplication of  $number1 * $number2 = ".($number1*$number2);
+}else if($operator === '+'){
+    $result = "Addition of  $number1 + $number2 = ".($number1+$number2);
+}else if($operator === '-'){
+    $result = "Substraction of  $number1 - $number2 = ".($number1-$number2);
+}else{
+    echo "Invalid Operator!!!!!!!!!";
+}
+echo $result;
+
+}
 ?>
