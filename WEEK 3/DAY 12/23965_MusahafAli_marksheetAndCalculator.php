848119c896686fc371dc5b257b584da7b6b3d982
Musahaf Ali <musahafali43@gmail.com>
2026-04-20 23:30:06 +0500

    added all assignment 1 lecture 4 code

---
+++ WEEK 3/DAY 12/23965_MusahafAli_marksheetAndCalculator.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <form action="process.php" method="POST">
+        
+        <table cellspacing=2 cellpadding=10 border=2>
+            <tr>
+                <th colspan=2>EXTENDED MARKSHEET</th>
+            </tr>
+            <tr>
+                <th>Computer:</th>
+                <td><input type="text" name="computer"></td>
+            </tr>
+             <tr>
+                <th>Economics:</th>
+                <td><input type="text" name="economics"></td>
+            </tr>
+             <tr>
+                <th>Finance:</th>
+                <td><input type="text" name="finance"></td>
+            </tr>
+             <tr>
+                <th>Managment:</th>
+                <td><input type="text" name="managment"></td>
+            </tr>
+             <tr>
+                <th>Programming:</th>
+                <td><input type="text" name="programming"></td>
+            </tr>
+            <tr align=center>
+                
+                <td colspan=2><input type="submit" name="marksheet" value="marksheet"></td>
+            </tr>
+        </table>
+    </form>
+    </center>
+</body>
+</html>
---
+++ WEEK 3/DAY 12/process.php
@@ -0,0 +1,7 @@
+<?php
+echo $_POST['submit'];
+if($_POST["submit"] == "marksheet"){
+    echo "Hao baba hao";
+};
+
+?>
