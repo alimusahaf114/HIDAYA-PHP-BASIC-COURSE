ddbf578651790ba663c63016ef1e0c79a069e83a
Musahaf Ali <musahafali43@gmail.com>
2026-05-01 11:51:25 +0500

    I have practiced some previous lacture code

---
+++ TEST PRACTICE CODE/Array Practice Code/index.php
@@ -0,0 +1,48 @@
+<?php
+
+// echo "Hellow world";
+// $arr = [23,34,65, 7=> 98 ,2=>76,87, 1=>45];
+// $arr = [98,12=>67,67,98,56,8=>999 , 5=>78];
+// $arr = [45,98,67,1.1=>78 , 90 , 0.9=>999 , -1.4=>8765];
+// echo $arr;
+// print $arr;
+// echo print_r($arr)
+// $arr = [12,''=>12,98  , -7.2=>98  , -1=>99 , NULL => 0.1];
+// echo "<pre>";
+// print_r($arr);
+// echo "</pre>";
+// $arr = [1 => "A", "1" => "B", 1.5 => "C", true => "D"];
+// print_r($arr);
+// $arr = [NULL => "A", "" => "B", false => "C"];
+// print_r($arr);
+// $arr = [2 => "X", 3 => "Y", 5 => "Z"];
+// echo count($arr);
+// $arr = [10 => "A", "10abc" => "B", 10.9 => "C"];
+// print_r($arr);
+// $arr = [];
+// $arr[] = "A";
+// $arr[2] = "B";
+// $arr[] = "C";
+
+// print_r($arr);
+// $arr = [1 => "A", 2 => "B", 3 => "C"];
+// $arr[true] = "D";
+// $arr[false] = "E";
+
+// print_r($arr);
+// $arr = ["" => "A", 0 => "B", "0" => "C"];
+// print_r($arr);
+// $arr = [1.2 => "A", 1.8 => "B", -1.2 => "C"];
+// print_r($arr);
+// $arr = ["5" => "A", 5 => "B", 5.7 => "C"];
+// print_r($arr);
+// $arr = [];
+// $arr[-1] = "A";
+// $arr[] = "B";
+// $arr[] = "C";
+
+// print_r($arr);
+
+// Hello My name 
+
+?>
---
+++ TEST PRACTICE CODE/Dynamic Form Practice/page2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dynamic form page 1</title>
+</head>
+<body>
+    <center>
+    <form action="page2.php" method="POST" >
+        <table border="1"  cellpadding=10>
+            <?php 
+            $num = $_POST['fields'];
+            ?>
+            <tr>
+               <th>S_No</th>
+               <th>Name</th>
+               <th>Gender</th>
+               <th>country</th>
+            </tr>
+            <?php 
+            for($i = 1 ; $i <= $num ; $i++){
+            ?>
+
+            <tr>
+                <td style="font-weight: bold;"><?php echo $i  ?></td>
+                <td><input type="text" name="first_name<?php echo $i ?>" ></td>
+                <td>
+                    Male: <input type="radio" name="gender<?php echo $i ?>" value="male">
+                    Female: <input type="radio" name="gender<?php echo $i ?>" value="female">
+                </td>
+                <td>
+                    <select name="country<?php echo $i ?>">
+                        <option value="pakistan">pakistan</option>
+                        <option value="Iran">Iran</option>
+                        <option value="Iraq">Iraq</option>
+                    </select>
+                </td>
+            </tr>
+
+
+            <?php
+            }
+            ?>
+            <tr align=center>
+                <td colspan=4>
+                    <input type="submit" name="submit" >
+                </td>
+            </tr>
+        </table>
+        <td><input type="hidden" name="fields" value=<?php echo $num ?>></td>
+    </form>
+</center>
+</body>
+</html>
