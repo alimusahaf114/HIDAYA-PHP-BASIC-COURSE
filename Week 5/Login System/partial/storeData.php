dff34fe618596229a50c07c7f5d6fc60755101be
Musahaf Ali <musahafali43@gmail.com>
2026-05-17 13:33:45 +0500

    created successfully

--- Week 5/Login System/partial/storeData.php
+++ Week 5/Login System/partial/storeData.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+$_SESSION['username'] = $_POST['username']
+if($_POST['username'] )
+$_SESSION['password'] = $_POST['username']
 echo "<pre>";
 print_r($_POST);
 echo "</pre>";
--- Week 5/Login System/signup.php
+++ Week 5/Login System/signup.php
@@ -40,21 +40,39 @@
     <?php
     require('partial/_nav.php')
     ?>
+    <?php
+            session_start();
+            $error = null;
+            if(isset($_POST['submit']))
+            {
+            $_SESSION['username'] = $_POST['useremail'];
+
+            if($_POST['password'] == $_POST['confirmPass'])
+            {
+            $_SESSION['password'] = $_POST['password'];
+            }else{
+                $error =  "<p style='color: red;'> Wrong Password </p>";
+            }}
+    ?>
     <div  class="container">
         <h1>Sign Up to Our Website</h1>
-        <form action="./partial/storeData.php" method="POST">
+        <form action="signup.php" method="POST">
             <div class="form-group">
                 <label for="username">Username</label>
-                <input class="inp" type="email" name="useremail" id="useremail" placeholder="Type your username">
+                <input class="inp" type="email" name="useremail" id="useremail" value="<?php if($error) echo $_POST['useremail']; ?>" placeholder="Type your username">
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input class="inp" type="password" name="password" id="userpassword" placeholder="Type your password">
+                <input class="inp" type="password" name="password" id="userpassword" value="<?php if($error) echo $_POST['password']; ?>" placeholder="Type your password" required>
             </div>
             <div class="form-group">
                 <label for="password">Confirm Password</label>
-                <input class="inp" type="password" name="confirmPass" id="userpassword" placeholder="confirm password">
-                <small style="display:block">Make sure to type same password as above</small>
+                <input class="inp" type="password" name="confirmPass" id="userpassword" placeholder="confirm password" required>
+                <small style="display:block"><?php 
+                if($error){
+                    echo $error;
+                }
+                ?></small>
             </div>
             <div class="form-group">
                 <input type="submit" name="submit" value="Signup" id="btn">
